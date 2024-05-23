a9402e357899562a47272ed6aa1fa0141f354107
Lennwmedia <lennwmedia@gmail.com>
2024-05-23 12:21:38 -0600

    🐛 new traduccion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -66,7 +66,10 @@ function Home() {
           ref={h11}
           className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
         >
-          Hi,👋<br></br>My Name is<br></br>
+          <FormattedMessage 
+           id="mainMessageHello"
+           defaultMessage="Hi"
+          />,👋<br></br><FormattedMessage id="mainName" defaultMessage="My Name is" /><br></br>
         </h1>
         <h1
           ref={h12}
